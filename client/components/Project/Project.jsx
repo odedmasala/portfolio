4ec17a1986750cbdb0f6b3da881fd27f5f6b6bbd
odedmasala <odedmasala6@gmail.com>
2022-10-25 01:51:05 +0300

    update

--- client/components/Project/Project.jsx
+++ client/components/Project/Project.jsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 const Project = ({ proData }) => {
   // console.log(proData.backgroundImg);
   return (
-    <div>
-      <div className="Project-item">
-        <div className="project-container">
+    <div className="Project-item">
+      <div>
+        <div className="background-container">
           <div className="background-image bg-black/70" />
           <Image
             className="image"
@@ -17,30 +17,30 @@ const Project = ({ proData }) => {
             src={`/${proData.backgroundImg}`}
             alt="/"
           />
-          <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <div className="background-title">
             <h2 className="py-2">{proData.title}</h2>
             <h3>{proData.addTech}</h3>
           </div>
         </div>
 
-        <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 md xl:h-[43vh] gap-8 py-8">
-          <div className="col-span-4">
+        <div className="details-container md:grid-cols-5 xl:h-[43vh]">
+          <div className="description-container col-span-4">
             <p>Project</p>
             <h2>Overview</h2>
             <p>{proData.proDescription}</p>
-            <a href={proData.liveDemo} target="_blank" rel="noreferrer">
-              <button className="px-8 py-2 mt-4 mr-8">Code</button>
+            <a href={proData.proHref} target="_blank" rel="noreferrer">
+              <button className="px-8 py-2 mt-4 mr-8 btn info-btn ml-6">Code</button>
             </a>
             <a href={proData.liveDemo} target="_blank" rel="noreferrer">
-              <button className="px-8 py-2 mt-4">Demo</button>
+              <button className="px-8 py-2 mt-4 btn info-btn ml-6">Demo</button>
             </a>
           </div>
-          <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
+          <div className="Technologies-container col-span-4 md:col-span-1 shadow-lg shadow-gray-400 dark:shadow-gray-800 border border-[#1211116d] rounded-xl py-4">
             <div className="p-2">
               <p className="text-center font-bold pb-2">Technologies</p>
               <div className="grid grid-cols-3 md:grid-cols-1">
                 {proData?.addTechList.map((elem) => (
-                  <p className="text-gray-600 py-2 flex items-center">
+                  <p className=" py-2 flex items-center">
                     <RiRadioButtonFill className="pr-1" /> {elem}
                   </p>
                 ))}
--- client/utils/projectsList.js
+++ client/utils/projectsList.js
@@ -1,45 +1,67 @@
-export   const projectsList = [
-  
+export const projectsList = [
   {
     proName: "YouTubeClone",
     title: "you Tube app",
     backgroundImg: "youtube2.jpg",
     tech: "React JS",
-    addTech:"React JS / mui / rapid API",
-    addTechList : ["React JS","Mui","rapid API"],
-    proDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, quos. Corrupti illum facere aut necessitatibus minus vel quis temporibus reprehenderit, accusamus deserunt dolor? Quas inventore deleniti debitis consequatur, qui veniam.`,
-    proHref : "https://github.com/odedmasala/react_youtuber_clone",
-    liveDemo :"https://vermillion-torte-8f01c4.netlify.app/"
+    addTech: "React JS / mui / rapid API",
+    addTechList: ["React JS", "Mui", "rapid API"],
+    proDescription: `I built this application in React JS and is hosted on GitHub pages.
+    This app features user authentication with firebase as well as the
+    firestore cloud storage database. This application is pulling movie
+    data from an the IMDB movie API and displaying different categories.
+    It features horizontal sliding and a featured selection. The
+    useContext hook is also being implemented for app-wide state
+    management.`,
+    proHref: "https://github.com/odedmasala/react_youtuber_clone",
+    liveDemo: "https://vermillion-torte-8f01c4.netlify.app/",
   },
   {
     proName: "YouTubeClone",
     title: "you Tube app",
     backgroundImg: "youtube2.jpg",
     tech: "React JS",
-    addTech:"React JS / mui / rapid API",
-    proDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, quos. Corrupti illum facere aut necessitatibus minus vel quis temporibus reprehenderit, accusamus deserunt dolor? Quas inventore deleniti debitis consequatur, qui veniam.`,
-    proHref : "https://github.com/odedmasala/react_youtuber_clone",
-    liveDemo :"https://vermillion-torte-8f01c4.netlify.app/"
+    addTech: "React JS / mui / rapid API",
+    proDescription: `I built this application in React JS and is hosted on GitHub pages.
+    This app features user authentication with firebase as well as the
+    firestore cloud storage database. This application is pulling movie
+    data from an the IMDB movie API and displaying different categories.
+    It features horizontal sliding and a featured selection. The
+    useContext hook is also being implemented for app-wide state
+    management.`,
+    proHref: "https://github.com/odedmasala/react_youtuber_clone",
+    liveDemo: "https://vermillion-torte-8f01c4.netlify.app/",
   },
   {
     proName: "YouTubeClone",
     title: "you Tube app",
     backgroundImg: "youtube2.jpg",
     tech: "React JS",
-    addTech:"React JS / mui / rapid API",
-    proDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, quos. Corrupti illum facere aut necessitatibus minus vel quis temporibus reprehenderit, accusamus deserunt dolor? Quas inventore deleniti debitis consequatur, qui veniam.`,
-    proHref : "https://github.com/odedmasala/react_youtuber_clone",
-    liveDemo :"https://vermillion-torte-8f01c4.netlify.app/"
+    addTech: "React JS / mui / rapid API",
+    proDescription: `I built this application in React JS and is hosted on GitHub pages.
+    This app features user authentication with firebase as well as the
+    firestore cloud storage database. This application is pulling movie
+    data from an the IMDB movie API and displaying different categories.
+    It features horizontal sliding and a featured selection. The
+    useContext hook is also being implemented for app-wide state
+    management.`,
+    proHref: "https://github.com/odedmasala/react_youtuber_clone",
+    liveDemo: "https://vermillion-torte-8f01c4.netlify.app/",
   },
   {
     proName: "YouTubeClone",
     title: "you Tube app",
     backgroundImg: "youtube2.jpg",
     tech: "React JS",
-    addTech:"React JS / mui / rapid API",
-    proDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, quos. Corrupti illum facere aut necessitatibus minus vel quis temporibus reprehenderit, accusamus deserunt dolor? Quas inventore deleniti debitis consequatur, qui veniam.`,
-    proHref : "https://github.com/odedmasala/react_youtuber_clone",
-    liveDemo :"https://vermillion-torte-8f01c4.netlify.app/"
+    addTech: "React JS / mui / rapid API",
+    proDescription: `I built this application in React JS and is hosted on GitHub pages.
+    This app features user authentication with firebase as well as the
+    firestore cloud storage database. This application is pulling movie
+    data from an the IMDB movie API and displaying different categories.
+    It features horizontal sliding and a featured selection. The
+    useContext hook is also being implemented for app-wide state
+    management.`,
+    proHref: "https://github.com/odedmasala/react_youtuber_clone",
+    liveDemo: "https://vermillion-torte-8f01c4.netlify.app/",
   },
-  ];
-  
+];
