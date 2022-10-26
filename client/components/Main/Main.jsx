59047387a1dbc66aed375de70699dab459a097d5
odedmasala <odedmasala6@gmail.com>
2022-10-26 21:56:11 +0300

    update

--- client/components/Main/Main.jsx
+++ client/components/Main/Main.jsx
@@ -10,8 +10,8 @@ const Main = () => {
             <h1>
               Hi, I&apos;m <span className="text-[#61954a]">Oded</span>
             </h1>
-            <h1>full stack developer</h1>
-            <p>I&apos;m focusing on looking for a fullstack/frontend position. I have a strong desire to prove myself and contribute to the team.</p>
+            <h1>Full Stack Developer</h1>
+            {/* <p>I&apos;m focusing on looking for a fullstack/frontend position. I have a strong desire to prove myself and contribute to the team.</p> */}
             <div className="connect-container">
               {connection?.map((elem, index) => (
                 <BtnList logoBtn={elem} key={index} />
