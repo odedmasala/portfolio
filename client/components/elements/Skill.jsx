df368e0ec840cfb4ceeb96e6272bfefd90b75eeb
odedmasala <odedmasala6@gmail.com>
2022-10-27 15:45:11 +0300

    fix responsive design

--- client/components/elements/Skill.jsx
+++ client/components/elements/Skill.jsx
@@ -5,9 +5,9 @@ const Skill = ({ iconObject }) => {
   return (
     <>
       <div className="p-6 shadow-md rounded-xl dark:shadow-gray-700 hover:scale-105 ease-in duration-300">
-        <div className="grid grid-cols-2 gap-4 justify-center items-center">
-          <div className="text-6xl">{iconObject.icon}</div>
-          <div className="flex flex-col items-center justify-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center items-center">
+          <div className="text-5xl md:text-7xl flex justify-center">{iconObject.icon}</div>
+          <div className="flex flex-col items-center justify-center text-center">
             <h3>{iconObject.name}</h3>
           </div>
         </div>
