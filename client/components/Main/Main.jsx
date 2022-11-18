14de563957d0cd3c3edfc6d55658a565ab0c05d2
odedmasala <odedmasala6@gmail.com>
2022-11-18 20:01:00 +0200

    End of editing

--- client/components/Main/Main.jsx
+++ client/components/Main/Main.jsx
@@ -8,9 +8,9 @@ const Main = () => {
         <div className="main-container">
           <div>
             <h1>
-              Hi, I&apos;m <span className="text-[#61954a]">Oded</span>
+              Hi &#128075;, I&apos;m <span className="text-[#61954a]">Oded</span>
             </h1>
-            <h1>Full Stack Developer</h1>
+            <h1>Software Developer</h1>
             {/* <p>I&apos;m focusing on looking for a fullstack/frontend position. I have a strong desire to prove myself and contribute to the team.</p> */}
             <div className="connect-container">
               {connection?.map((elem, index) => (
